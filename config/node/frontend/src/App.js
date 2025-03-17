2f7873fbbe862f426ca90cc5f66cb5e55b45293f
kingastaszynska <kinga.staszynska@student.wat.edu.pl>
2025-03-17 18:40:28 +0100

    wip: add docker-compose and docker file configuration for the frontend of app. Generic code can be considered as white labeled product

---
+++ config/node/frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return;
+  <div className="App">
+    <h1>Geoinformatyka rządzi! Ouyeah!</h1>
+  </div>;
+}
+
+export default App;
