a2226157ef971c51b4ae894bb70d2cbee01f85f8
kingastaszynska <kinga.staszynska@student.wat.edu.pl>
2025-03-31 18:25:32 +0200

    wip: add services page

--- config/node/frontend/src/home/Home.js
+++ config/node/frontend/src/home/Home.js
@@ -4,20 +4,20 @@ import zdjecie from "./01_mapa_wat.png";
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <div className="home_top">
         <div>O projekcie</div>
       </div>
       <div className="home_bottom">
         <div className="home_left">
-          <ing className="home_logo" src={zdjecie} alt="logo" />
+          <img className="home_logo" src={zdjecie} alt="logo" />
         </div>
         <div className="home_right">
           <h1 className="home_title">
-            <spam style={{ color: "#276cd3", opacity: "0.75" }}>GEO</spam>Portal
+            <span style={{ color: "#276CD3", opacity: 0.75 }}>GEO</span>Portal
           </h1>
           <div className="home_subtitle">
-            Strona internetowa z interaktywną mapą
+            Stona internetowa z interaktywną mapą
           </div>
           <button className="home_button">WCHODZĘ!</button>
         </div>
